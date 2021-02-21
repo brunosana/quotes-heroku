88ccd8a3866042021371e9698bce24dd5a83824f
brunosana <brunosa@dcomp.ufs.br>
2021-02-21 15:49:20 -0300

    feat: Create User Migration

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 2. PostgreSQL para o banco de dados
 3. Heroku para o deploy
 4. ESlint e Prettier para formatação de código
-
+5. Yarn como gerenciador de pacotes
 
 
 ### Para usar o ESLint
@@ -21,4 +21,41 @@
     "source.fixAll.eslint": true
 }
 ```
+4. Reinicie o VSCode e estará pronto.
 
+**Para outros editores, por favor consulte a sua respectiva documentação.**
+
+## Executando o projeto
+
+### Banco de dados
+
+
+1. Com docker
+
+Para instalar o container do postgresql com os dados corretamente, utilize a seguinte linha de comando:
+```
+docker run --name quotes -e POSTGRES_USER=quotes_admin -e POSTGRES_PASSWORD=quotes_application -p 5432:5432 -d postgres
+```
+
+2. Para outros formados de banco de dados, basta seguir a documentação criando um banco `quotes` com usuario e password a sua escolha.
+3. Na pasta raiz do projeto, criar um arquivo `.env` contendo:
+```
+PORT=3333
+TYPEORM_CONNECTION = postgres
+TYPEORM_HOST = localhost
+TYPEORM_PORT = 5432
+TYPEORM_USERNAME = usuario_postgres_criado
+TYPEORM_PASSWORD = senha_postgres_criada
+TYPEORM_DATABASE = quotes
+TYPEORM_ENTITIES = src/models/*.ts
+TYPEORM_ENTITIES_DIR = src/models/
+TYPEORM_MIGRATIONS = src/database/migrations/*.ts
+TYPEORM_MIGRATIONS_DIR = src/database/migrations
+TYPEORM_SYNCHRONIZE = true
+```
+
+
+### Instalando as dependências
+
+1. No terminal da pasta, inserir `yarn` para instalar as dependências.
+2. Executar a aplicação com `yarn dev:server`
---
+++ src/database/migrations/1613926149667-createUserEntity.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createUserEntity1613926149667
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'username',
+                        type: 'varchar',
+                        isUnique: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+}
